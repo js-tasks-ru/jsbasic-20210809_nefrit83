0ca46b47e663d4f4be80dd136af8e1e6ec62c632
Elena <rioritka@gmail.com>
2021-08-15 14:01:24 +0300

    4th task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,9 @@
 function ucFirst(str) {
   // ваш код...
+  if (str.length > 0) {
+    let newStr = str[0].toUpperCase() + str.slice(1, str.length);
+    return newStr;
+  } else {
+    return "";
+  }
 }
