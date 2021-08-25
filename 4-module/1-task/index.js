822fb4227b7fe53b4e26c8d57e456c9735d4ba83
Elena <rioritka@gmail.com>
2021-08-25 15:16:18 +0300

    4-module.1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let newList = document.createElement("ul");
+  document.body.append(newList);
+
+  for (let i = 0; i < friends.length; i++) {
+    let nextFriend = document.createElement("li");
+    nextFriend.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+    newList.append(nextFriend);
+  }
+  return newList;
 }
