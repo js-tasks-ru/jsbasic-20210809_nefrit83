42dde6efebacaa2708432313f4ee3f1288082055
Elena <rioritka@gmail.com>
2021-09-01 21:33:21 +0300

    5-module.3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
   // ваш код...
+  let carouselArrow = document.querySelector(".carousel");
+  let rightButton = document.querySelector(".carousel__arrow_right");
+  let leftButton = document.querySelector(".carousel__arrow_left");
+  let carousel = document.querySelector(".carousel__inner");
+  let slideWidth = carousel.offsetWidth;
+  let index = 0;
+
+  function indexCounter(n) {
+    if (n === 0) {
+      leftButton.style.display = "none";
+    } else {
+      leftButton.style.display = "";
+    }
+    if (n === 3) {
+      rightButton.style.display = "none";
+    } else {
+      rightButton.style.display = "";
+    }
+  }
+
+  indexCounter(index);
+  
+  carouselArrow.addEventListener("click", function (event) {
+    let target = event.target;
+    if (target.closest(".carousel__arrow_right")) {
+      index++;
+      carousel.style.transform = `translateX(${-slideWidth * index}px)`;
+      indexCounter(index);
+    }
+    if (target.closest(".carousel__arrow_left")) {
+      index--;
+      carousel.style.transform = `translateX(${-slideWidth * index}px)`;
+      indexCounter(index);
+    }
+  });
 }
